84636cb349ad0c71d2bf83c237135af756a7d87c
kellykindla <kellykindla@gmail.com>
2022-02-04 17:22:28 -0600

    deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -12,3 +12,13 @@ lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,
 summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_DF)
 )
 
+
+##Deliverable 2 
+# import and read in the suspension_coil.csv file 
+Suspension_Coil <- read.csv("Suspension_Coil.csv")
+
+#create total_summary df using summarize() to get the mean, median, variance, and sd of the PSI column 
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+#create lot_summary df using the group_by() and summarize() functions to group each lot by the mean, meidan,variance, and sd of PSI
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = "keep")
